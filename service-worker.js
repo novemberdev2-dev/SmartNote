62091a14605809c606d72b27e465d20e9e9bdf3e
novemberdev2-dev <november.dev2@gmail.com>
2026-08-26 22:34:09 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,30 @@
-const CACHE_NAME = "smartnote-cache-v1";
+const CACHE_NAME = "smartnote-cache-v2";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./SmartNote-icon.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
   );
 });
 
